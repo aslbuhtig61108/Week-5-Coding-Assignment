220759900489b14ca12f4782225f97d569d02f4f
Ed Locquiao <nilatian.15@gmail.com>
2022-07-30 22:09:14 -0700

    comments added test completed. Screenshots then submit final pdf and push to repository

--- Week5 Coding Assignment/src/Logger.java
+++ Week5 Coding Assignment/src/Logger.java
@@ -2,7 +2,10 @@
 public interface Logger {
 
 	// The classes that implements these abstract methods will use
-	// the String parameter in the manner each has been implemented 
+	// the String parameter in the manner each class has been implemented
+	// This means that both our AsteriskLogger and SpacedLogger classes 
+	// access both methods, but their implementations can be completely different
+	// 
 	public void Log(String info);
 	public void Error(String error);
 	
